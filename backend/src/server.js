2c5c5a7ce9592553ba9814e45ee74abb131d2bfd
SiddharthNITSURAT <shsheth2006@gmail.com>
2025-12-30 21:02:28 +0530

    fixed routes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import healthRoutes from "./src/routes/healthRoutes.js";
+import healthRoutes from "./routes/healthRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 app.use("/api/health", healthRoutes);
