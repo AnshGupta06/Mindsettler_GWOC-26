bde8b0db13fc73a6d07ccfd6a5d862e481df3a3a
Ansh Gupta <anshgupta2706@gmail.com>
2026-01-07 21:12:28 +0530

    deployment trigger

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,6 @@ app.use(helmet());
 
 const allowedOrigins = [
   "http://localhost:3000", // Next.js Localhost
-  // "https://mindsettler.vercel.app" <--- UNCOMMENT & ADD YOUR LIVE URL HERE LATER
   "https://mindsettler-bypb.vercel.app"
 ];
 
