a5ba6d9a02bd1bd89442e51c98241e1414a30352
SiddharthNITSURAT <shsheth2006@gmail.com>
2026-01-14 00:26:56 +0530

    Made more pages SSR Components

---
+++ frontend/app/(main)/HomeClient.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import HeroSection from "./components/home/HeroSection";
+import AboutPreview from "./components/home/AboutPreview";
+import JourneySection from "./components/home/JourneySection";
+import Differentiators from "./components/home/Differentiators";
+import PersonalPathSection from "./components/home/PersonalPath";
+import WorkplaceWellnessSection from "./components/home/Workplace";
+import HowItWorks from "./components/home/HowItWorks";
+import FAQSection from "./components/home/FAQSection";
+import SectionDivider from "./components/common/SectionDivider";
+
+export default function HomeClient() {
+  return (
+    <main className="min-h-screen bg-[#F9F6FF]">
+      <HeroSection />
+      <SectionDivider />
+      <AboutPreview />
+      <SectionDivider />
+      <HowItWorks />
+      <SectionDivider />
+      <JourneySection />
+      <SectionDivider />
+      <Differentiators />
+      <SectionDivider />
+      <PersonalPathSection />
+      <SectionDivider />
+      <WorkplaceWellnessSection />
+      <SectionDivider />
+      <FAQSection />
+    </main>
+  );
+}
